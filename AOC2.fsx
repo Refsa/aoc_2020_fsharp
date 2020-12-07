a5553c774f5235b228d2cd3ba187950e78f1ccbf
Ole <refsa22@gmail.com>
2020-12-07 14:18:24 +0100

    full solution for part 2

--- AOC2.fsx
+++ AOC2.fsx
@@ -12,6 +12,12 @@ let unwrap_result (parser: ParserResult<'a, 'b>): 'a =
     | Success (result, _, _) -> result
     | Failure (error, _, _) -> Unchecked.defaultof<'a>
 
+let get_some_count (target: 'a option list) =
+    target
+    |> List.filter (fun a -> a.IsSome)
+    |> List.length
+    |> fun a -> a.ToString()
+
 let run input =
     let parse_range =
         pipe2 (pstring "" >>. pint32) (skipChar '-' >>. pint32) (fun a b -> (a, b))
@@ -30,24 +36,23 @@ let run input =
             | 0
             | _ -> None
 
-    let password_parser_2 (min, max) wanted password =
+    let password_parser_2 (min: int, max: int) wanted password =
         password
-        |> String.filter (fun a -> a = wanted)
+        |> String.mapi (fun i a -> 
+            match (i + 1) with
+            | i when (i = min || i = max) && a = wanted -> 'X'
+            | _ -> 'O'
+        )
+        |> String.filter (fun a -> a = 'X')
         |> String.length
         |> function
-            | x when x >= min && x <= max -> Some(x)
-            | 0
+            | 1 -> Some(0)
             | _ -> None
 
-    let parse =
-        run (many (pipe3 parse_range parse_char parse_rest password_parser_1)) input
+    let parse1 =
+        run (many (pipe3 parse_range parse_char parse_rest password_parser_1)) input |> unwrap_result |> get_some_count
 
-    let result =
-        match parse with
-        | Success (result, _, _) -> result
-        | Failure (error, _, _) -> []
+    let parse2 =
+        run (many (pipe3 parse_range parse_char parse_rest password_parser_2)) input |> unwrap_result |> get_some_count
 
-    result
-    |> List.filter (fun a -> a.IsSome)
-    |> List.length
-    |> fun a -> a.ToString()
+    sprintf "Part1: %s\nPart2: %s" parse1 parse2
